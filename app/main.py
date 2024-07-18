8301fc8d2f97076e1b544d7f9b385725c3ac659a
Samyak Sharma <sammi.bgas@gmail.com>
2024-07-18 16:45:46 +0530

    1

--- app/main.py
+++ app/main.py
@@ -17,3 +17,6 @@ def Send_Mail(post: schemas.EmailRequest):
     Data = schemas.EmailRequest(**post.dict())
     models.send_email(Data.Head, Data.Body, Data.email)
     return "Email Sent"
+
+
+print("helloworld")
